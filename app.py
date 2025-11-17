0adcd8a23dfde75c61096c8c07d73432db937baf
Mariana Simion <marianasimiondossantos@gmail.com>
2025-11-17 14:35:25 -0300

    :bricks: Aplicação do método POST na entidade Condominio

--- app.py
+++ app.py
@@ -55,6 +55,59 @@ def get_condominios():
         if conexao:
             conexao.close()
             
+@app.route('/condominios', methods=['POST'])
+
+def create_condominios():
+    try:
+        dados = request.json
+        cnpj = dados.get('cnpj')
+        nome = dados.get('nome')
+        logradouro = dados.get('logradouro')
+        bairro = dados.get('bairro')
+        cidade = dados.get('cidade')
+        uf = dados.get('uf')
+        cep = dados.get('cep')
+
+    except Exception as e:
+        return jsonify(erro=f"Erro ao ler os dados JSON:{e}"), 400
+        # # # # # #  # # DOING # # # # # # # #
+
+    if not cnpj or not nome:
+        return jsonify(erro="CNPJ e Nome são campos obrigatórios."),400
+    
+    conexao, cursor = get_db_connection()
+
+    if conexao is None:
+        return jsonify(erro = "Não foi possível ligar a base de dados"), 500
+    
+    try:
+        comando = f'INSERT INTO condominio (cnpj, nome, logradouro, bairro, cidade, uf, cep ) VALUES (%s, %s, %s, %s, %s, %s, %s)'
+        
+        valores = (cnpj, nome, logradouro, bairro, cidade, uf, cep ) 
+        cursor.execute(comando, valores)
+
+        conexao.commit()
+
+        return make_response(
+            jsonify(
+                mensagem = f"Condomínio '{nome}'criado com sucesso!",
+                dados = dados
+            ),
+            201 #HTTP status "Created"
+        )
+    
+    except mysql.connector.Error as err:
+        #se der erro, o commit não acontece
+        conexao.rollback()
+        return jsonify(erro=f"Erro ao consultar: {err}"), 400
+    
+    finally:
+        # 3. Fechar a conexão e o cursor DEPOIS de os usar
+        if cursor:
+            cursor.close()
+        if conexao:
+            conexao.close()
+            
 
 if __name__ == '__main__':
     print("Iniciando o servidor Flask em http://127.0.0.1:5000/")
