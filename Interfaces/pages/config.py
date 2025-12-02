be7ed37a29679c3c9784eb126a3c0c574bd49145
Mariana Simion <marianasimiondossantos@gmail.com>
2025-12-02 09:29:11 -0300

    :refactor: Reoorganização da camda de enlace

--- Interfaces/pages/config.py
+++ Interfaces/pages/config.py
@@ -5,7 +5,7 @@
 #DB_PASSWORD = '8192'
 #DB_NAME = 'condominio'
 
-DB_HOST = 'localhost'
-DB_USER = 'Mariana'
-DB_PASSWORD = 'Banco.123'
-DB_NAME = 'condominio'
+#DB_HOST = 'localhost'
+#DB_USER = 'Mariana'
+#DB_PASSWORD = 'Banco.123'
+#DB_NAME = 'condominio'
