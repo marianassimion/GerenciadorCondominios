e7248fb1669b392823cc2a328c0c575f7b2d944b
Mariana Simion <marianasimiondossantos@gmail.com>
2025-12-03 16:57:10 -0300

    :sparkles: Atualização do script para implementação do procedure hehe

--- database/script_relacionamentos.sql
+++ database/script_relacionamentos.sql
@@ -141,3 +141,32 @@ CREATE TABLE AREA_COMUM (
     CONSTRAINT FK_area_comum_condominio 
         FOREIGN KEY (condominio_cnpj) REFERENCES CONDOMINIO(cnpj)
 );
+
+
+DELIMITER $$
+
+CREATE PROCEDURE calcular_media_salario(
+    OUT media_salario DECIMAL(10,2)
+)
+BEGIN
+    SELECT AVG(salario) INTO media_salario
+    FROM empregado;
+END $$
+
+DELIMITER ;
+DELIMITER $$
+
+CREATE PROCEDURE calcular_media_salario_por_condominio(
+    IN p_cnpj VARCHAR(14),
+    OUT media_salario DECIMAL(10,2)
+)
+BEGIN
+    SELECT AVG(salario) INTO media_salario
+    FROM empregado
+    WHERE condominio_cnpj = p_cnpj;
+END $$
+
+DELIMITER ;
+
+CALL calcular_media_salario_por_condominio(3, @media);
+SELECT @media;
