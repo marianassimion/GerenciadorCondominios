241b368f15fd9e792d7a6330cdeb91bdd41868a7
Mariana Simion <marianasimiondossantos@gmail.com>
2025-12-03 23:10:56 -0300

    Padronizando arquivos

--- Interfaces/pages/config.py
+++ Interfaces/pages/config.py
@@ -1,11 +1,11 @@
 #config.py
 
-DB_HOST = 'localhost'
-DB_USER = 'root'
-DB_PASSWORD = '8192'
-DB_NAME = 'condominio'
-
 #DB_HOST = 'localhost'
-#DB_USER = 'Mariana'
-#DB_PASSWORD = 'Banco.123'
+#DB_USER = 'root'
+#DB_PASSWORD = '8192'
 #DB_NAME = 'condominio'
+
+DB_HOST = 'localhost'
+DB_USER = 'Mariana'
+DB_PASSWORD = 'Banco.123'
+DB_NAME = 'condominio'
