3b4c64d6f11ac324e7576631883669a3243b4628
rute-fernds <rute.a.fernds@gmail.com>
2025-12-03 21:29:16 -0300

    Comentário

--- Interfaces/pages/config.py
+++ Interfaces/pages/config.py
@@ -1,9 +1,9 @@
 #config.py
 
-DB_HOST = 'localhost'
-DB_USER = 'root'
-DB_PASSWORD = '8192'
-DB_NAME = 'condominio'
+#DB_HOST = 'localhost'
+#DB_USER = 'root'
+#DB_PASSWORD = '8192'
+#DB_NAME = 'condominio'
 
 #DB_HOST = 'localhost'
 #DB_USER = 'Mariana'
